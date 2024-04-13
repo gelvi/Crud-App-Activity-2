8eb22b88dd40b2ac18e4c019b432749d213a66af
Jeryll Serrano <132729949+Zetsu123@users.noreply.github.com>
2024-04-13 17:29:46 +0800

    Create DeleteProduct.vue

---
+++ src/components/DeleteProduct.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="confirmDelete">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    confirmDelete() {
+      if (confirm("Are you sure you want to delete this product?")) {
+        this.$emit("delete");
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add your scoped CSS styles here */
+</style>
